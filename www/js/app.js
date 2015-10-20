0dd45a87b3eec446cb6b6dc0b3cbd9c16497f9a9
Daniel Staudigel <dstaudigel@gmail.com>
2015-10-19 21:04:25 -0700

    videogular

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,14 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
-
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services',
+  "ngSanitize",
+  "com.2fdevs.videogular",
+  "com.2fdevs.videogular.plugins.controls",
+  "com.2fdevs.videogular.plugins.overlayplay",
+  "com.2fdevs.videogular.plugins.poster"
+]
+)
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -77,77 +83,8 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
       }
     }
   });
- /* .controller('MapController', function($scope, $cordovaGeolocation, $ionicloading){
-   document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-      //code goes here
-      $ionicLoading.show({
-        template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Finding your body in space!'
-      });
 
-      var posOption = {
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 0
-      };
-      $cordovaGeolocation,getCurrentPosition(posOptions).then(function (position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-
-        var myLatlng = new google.maps.LatLng(lat, long);
-
-        var myOptions = {
-          center: myLanlng,
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map=new google.maps.Map(document.getElementById("map"), mapOptions);
-        $scope.map = map;
-        $ionicLoading.hide();
-      }, function(err) {
-        $ionicLoading.hide();
-        console.log(err);
-      });
-      }
-  });
- */
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
 });
-
-//Videogular from http://www.videogular.com/tutorials/how-to-start/
-'use strict';
-angular.module('myAcro',
-		[
-			"ngSanitize",
-			"com.2fdevs.videogular",
-			"com.2fdevs.videogular.plugins.controls",
-			"com.2fdevs.videogular.plugins.overlayplay",
-			"com.2fdevs.videogular.plugins.poster"
-		]
-	)
-	.controller('HomeCtrl',
-		["$sce", function ($sce) {
-			this.config = {
-				sources: [
-					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-				],
-				tracks: [
-					{
-						src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-						kind: "subtitles",
-						srclang: "en",
-						label: "English",
-						default: ""
-					}
-				],
-				theme: "bower_components/videogular-themes-default/videogular.css",
-				plugins: {
-					poster: "http://www.videogular.com/assets/images/videogular.png"
-				}
-			};
-		}]
-	);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,40 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
+/* .controller('MapController', function($scope, $cordovaGeolocation, $ionicloading){
+  document.addEventListener("deviceready", onDeviceReady, false);
+   function onDeviceReady() {
+     //code goes here
+     $ionicLoading.show({
+       template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Finding your body in space!'
+     });
+
+     var posOption = {
+       enableHighAccuracy: true,
+       timeout: 20000,
+       maximumAge: 0
+     };
+     $cordovaGeolocation,getCurrentPosition(posOptions).then(function (position) {
+       var lat = position.coords.latitude;
+       var long = position.coords.longitude;
+
+       var myLatlng = new google.maps.LatLng(lat, long);
+
+       var myOptions = {
+         center: myLanlng,
+         zoom: 16,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+       };
+       var map=new google.maps.Map(document.getElementById("map"), mapOptions);
+       $scope.map = map;
+       $ionicLoading.hide();
+     }, function(err) {
+       $ionicLoading.hide();
+       console.log(err);
+     });
+     }
+ });
+*/
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -21,6 +55,21 @@ angular.module('starter.controllers', [])
     $scope.chat = Chats.get($stateParams.chatId);
   })
 
-  
+  .controller('HomeCtrl',
+		["$sce", function ($sce) {
+			this.config = {
+				sources: [
+					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
+					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+				],
+				theme: "lib/videogular-themes-default/videogular.css",
+				plugins: {
+					poster: "http://www.videogular.com/assets/images/videogular.png"
+				}
+			};
+		}]
+	)
+
   .controller('AccountCtrl', function($scope) {
   });
