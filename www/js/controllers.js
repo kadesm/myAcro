0b53c56e14c9e085559f7d6da046230c0351f306
Daniel Staudigel <dstaudigel@gmail.com>
2015-11-09 16:38:20 -0800

    Deleting Cordova

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,40 +2,6 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-/* .controller('MapController', function($scope, $cordovaGeolocation, $ionicloading){
-  document.addEventListener("deviceready", onDeviceReady, false);
-   function onDeviceReady() {
-     //code goes here
-     $ionicLoading.show({
-       template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Finding your body in space!'
-     });
-
-     var posOption = {
-       enableHighAccuracy: true,
-       timeout: 20000,
-       maximumAge: 0
-     };
-     $cordovaGeolocation,getCurrentPosition(posOptions).then(function (position) {
-       var lat = position.coords.latitude;
-       var long = position.coords.longitude;
-
-       var myLatlng = new google.maps.LatLng(lat, long);
-
-       var myOptions = {
-         center: myLanlng,
-         zoom: 16,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-       };
-       var map=new google.maps.Map(document.getElementById("map"), mapOptions);
-       $scope.map = map;
-       $ionicLoading.hide();
-     }, function(err) {
-       $ionicLoading.hide();
-       console.log(err);
-     });
-     }
- });
-*/
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -51,10 +17,52 @@ angular.module('starter.controllers', [])
   };
 })
 
-  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.chatId);
-  })
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
 
+
+.controller('MapController', function($scope, $cordovaGeolocation, $ionicLoading) {
+     
+    document.addEventListener("deviceready", onDeviceReady, false);
+     
+    function onDeviceReady() {
+         
+        $ionicLoading.show({
+            template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+        });
+         
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 0
+        };
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+             
+            var myLatlng = new google.maps.LatLng(lat, long);
+             
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };          
+             
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
+             
+            $scope.map = map;   
+            $ionicLoading.hide();           
+             
+        }, function(err) {
+            $ionicLoading.hide();
+            console.log(err);
+        });
+    }               
+})
+
+
+/* videogular stuff */
   .controller('HomeCtrl',
 		["$sce", function ($sce) {
 			this.config = {
@@ -63,13 +71,27 @@ angular.module('starter.controllers', [])
 					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
 					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
 				],
-				theme: "lib/videogular-themes-default/videogular.css",
+				tracks: [
+					{
+						src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+						kind: "subtitles",
+						srclang: "en",
+						label: "English",
+						default: ""
+					}
+				],
+			
 				plugins: {
 					poster: "http://www.videogular.com/assets/images/videogular.png"
 				}
 			};
 		}]
-	)
+	           )
 
-  .controller('AccountCtrl', function($scope) {
-  });
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
+
